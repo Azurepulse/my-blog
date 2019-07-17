ff8581ce04801873ff61fc7e2641e43bc2453e38
Azurepulse <submicroso@outlook.com>
2019-07-17 15:00:23 +0800

    7-17：

--- src/main/java/com/site/blog/my/core/entity/TestObject.java
+++ src/main/java/com/site/blog/my/core/entity/TestObject.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
  * @author ZTY
  */
 public class TestObject {
-    public TestObject() {
 
+    public TestObject() {
     }
 
-    public TestObject(boolean status, String message, HashMap<String,Object> additional) {
+    public TestObject(boolean status, String message, HashMap<String, Object> additional) {
         this.status = status;
         this.message = message;
         this.additional = additional;
@@ -18,7 +18,7 @@ public class TestObject {
 
     private boolean status;
     private String message;
-    private HashMap<String,Object> additional;
+    private HashMap<String, Object> additional;
 
     public boolean isStatus() {
         return status;
@@ -36,11 +36,11 @@ public class TestObject {
         this.message = message;
     }
 
-    public HashMap<String,Object> getAdditonal() {
+    public HashMap<String, Object> getAdditional() {
         return additional;
     }
 
-    public void setAdditonal(HashMap<String,Object> additional) {
+    public void setAdditional(HashMap<String, Object> additional) {
         this.additional = additional;
     }
 }
